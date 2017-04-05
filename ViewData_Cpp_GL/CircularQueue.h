a0a76050787fc4b9b14bd62e881045eb4fc62cff
ydar95 <ydar95@163.com>
2017-04-05 12:47:03 +0800

    添加 循环队列类

---
+++ ViewData_Cpp_GL/CircularQueue.h
@@ -0,0 +1,44 @@
+#ifndef _YDAR_HEAD_CIRCULAR_QUEUE_GL_H_
+#define _YDAR_HEAD_CIRCULAR_QUEUE_GL_H_
+
+#include<cstdint>
+#include<memory>
+template<class Ty>
+class CircularQueue
+{
+public:
+	CircularQueue(uint32_t In_left = 10, uint32_t In_right = 10) 
+		:_left(In_left), _right(In_right), _size(In_left + In_right + 1)
+	{
+		_ptr= std::shared_ptr<Ty>(new Ty[_size], std::default_delete<Ty[]>());
+		_index_crrent = 0;//
+	}
+	void moveLeft() {
+		_index_crrent--;
+		_index_updata = (((_index_crrent + 1 + _right)%_size)+ _size)%_size;
+	}
+	void moveRight() {
+		_index_crrent++;
+		_index_updata = (((_index_crrent - 1 - _left) % _size) + _size) % _size;
+	}
+	Ty&  getPrepareUpdataObject() {
+		return _ptr.get()[_index_updata];
+	}
+	~CircularQueue() {
+		NULL;
+	}
+
+	Ty& operator [](int32_t index) {
+		//auto tmp = (_index_crrent + index) % _size;
+		int32_t t_idx = (((_index_crrent+ index)%_size)+_size)%_size;
+		return _ptr.get()[t_idx];
+	}
+private:
+	int32_t _left;
+	int32_t _right;
+	int32_t _size;
+	int32_t _index_updata;
+	int32_t _index_crrent;
+	std::shared_ptr<Ty> _ptr;
+};
+#endif
